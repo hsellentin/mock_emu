aeb7befe810f55cc50e8c5e5174579e3808bd8df
Halston Sellentin <hsellentin@hygiena.com>
2024-02-06 12:10:00 -0800

    first

---
+++ mock_emu/mock_emu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        var currentMemoryMB = 100;
+        const int targetMemoryMB = 300; // Target memory in MB
+        const int sleepIntervalMS = 500; // Sleep interval between allocations in milliseconds
+        IntPtr pointer = Marshal.AllocHGlobal(1024);
+
+        while (currentMemoryMB < targetMemoryMB)
+        {
+            var sz = currentMemoryMB++ * 1024 * 1024;
+            pointer = Marshal.AllocHGlobal(sz);
+            Console.WriteLine($"Allocated {currentMemoryMB}MB of memory.");
+
+            // Simulate work by writing to the allocated memory
+            unsafe
+            {
+                byte* ptr = (byte*)pointer.ToPointer();
+                for (long j = 0; j < sz; j++)
+                {
+                    ptr[j] = 1;
+                }
+            }
+
+
+            Thread.Sleep(sleepIntervalMS); // Sleep for a short interval to control the rate of allocation
+            Marshal.FreeHGlobal(pointer);
+        }
+
+        Console.WriteLine("Test Complete");
+    }
+}
